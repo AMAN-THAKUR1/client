3c75ad4ab7c0c45e8a02afedaf54b59ca3945adb
Aman Thakur <79102837+AMAN-THAKUR1@users.noreply.github.com>
2025-02-04 13:45:46 +0530

    Update FileMessage.jsx

--- src/components/FileMessage.jsx
+++ src/components/FileMessage.jsx
@@ -14,7 +14,7 @@ function FileMessage({ item, name }) {
         <div className={`message flex ${item.name === name ? "justify-end" : "justify-start"} p-3`}>
             <div className="flex justify-around items-center gap-[20px] max-w-40 bg-blue-500 rounded-lg p-2">
                 <div><i className="fa fa-file-text" aria-hidden="true"></i></div>
-                <div>{item.fileName}</div>
+                <div className = "max-w-30 overflow-hidden">{item.fileName}</div>
                 <div className="cursor-pointer">
                     <button key={item.downloadUrl} onClick={()=>handleDownloadFile(item)}>
                         <i className="fa fa-download" aria-hidden="true" />
